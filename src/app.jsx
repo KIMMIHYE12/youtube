3cd2b7aba6ca00d679d447fa7f7f9532723531b9
sapiens12 <sapiens12@nate.com>
2022-10-31 01:55:59 +0100

    youtube Service

--- src/app.jsx
+++ src/app.jsx
@@ -1,27 +1,27 @@
-
-import { useEffect, useState } from 'react';
-import './app.css';
+import React, { useEffect, useState } from 'react';
+import styles from './app.module.css';
 import VideoList from './components/video_list/video_list';
+import SearchHeader from './components/search_header/search_header';
 
-function App() {
+function App({ youtube }) {
   const [videos, setVideos] = useState([]);
+  const search = query => {
+    youtube
+      .search(query) //
+      .then(videos => setVideos(videos));
+  };
 
-  useEffect(()=>{
-    const requestOptions = {
-      method: 'GET',
-      redirect: 'follow'
-    };
-    
-    fetch(
-      "https://youtube.googleapis.com/youtube/v3/videos?part=snippet&chart=mostpopular&maxResults=25&key=AIzaSyCbPl9Q2AF5MzHEix4H2I8SQ9Guw32NhYc", 
-      requestOptions
-      )
-      .then(response => response.json())
-      .then(result => setVideos(result.items))
-      .catch(error => console.log('error', error));
+  useEffect(() => {
+    youtube
+      .mostPopular() //
+      .then(videos => setVideos(videos));
   }, []);
-
-  return <VideoList videos={videos}/>;
+  return (
+    <div className={styles.app}>
+      <SearchHeader onSearch={search} />
+      <VideoList videos={videos} />
+    </div>
+  );
 }
 
 export default App;
